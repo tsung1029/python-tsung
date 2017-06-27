65d9a9da7cd8f4fe208e5ef314097fade86fba81
Han Wen <hanwen@ucla.edu>
2017-06-26 21:15:29 -0700

    add a script for Helmholtz decomposition of fields

--- analysis.py
+++ analysis.py
@@ -1,15 +1,24 @@
 import numpy as np
 import str2keywords
 from h5_utilities import *
+from arraymask import *
 from scipy.signal import hilbert
 
 
-def update_fft_axes(axes, forward=True):
+def update_fft_axes(axisdata, forward=True):
     if forward:
-        print 'forward transform'
+        freq = np.fft.fftshift(np.fft.fftfreq(axisdata.axis_numberpoints, d=axisdata.increment))
+        axisdata.increment = (freq[1] - freq[0]) * 2 * np.pi
+        axisdata.axis_min, axisdata.axis_max = freq[0] * 2 * np.pi, freq[-1] * 2 * np.pi
+        axisdata.attributes['NAME'][0] = 'K(' + axisdata.attributes['NAME'][0] + ')'
+        axisdata.attributes['LONG_NAME'][0] = 'K(' + axisdata.attributes['LONG_NAME'][0] + ')'
     else:
-        print 'backward transform'
-    return axes
+        ax = np.fft.ifftshift(np.fft.fftfreq(axisdata.axis_numberpoints, d=axisdata.increment))
+        axisdata.increment = (ax[1] - ax[0]) * 2 * np.pi
+        axisdata.axis_min, axisdata.axis_max = ax[0] * 2 * np.pi, ax[-1] * 2 * np.pi
+        axisdata.attributes['NAME'][0] = axisdata.attributes['NAME'][0][2:-1]
+        axisdata.attributes['LONG_NAME'][0] = axisdata.attributes['LONG_NAME'][0][2:-1]
+    return axisdata
 
 
 def analysis(data, ops_list, axes=None):
@@ -17,11 +26,13 @@ def analysis(data, ops_list, axes=None):
     Analysis data and change axes accordingly
     
     :param data: array_like data
-    :param ops_list: list of operations (str2keywords objects)
-    :param axes: list of axes (data_basic_axis objects) pass only the axes that need changes
+    :param ops_list: list of operations (str2keywords objects or strings)
+    :param axes: list of axes (data_basic_axis objects)
     :return: return processed data (and axes if provided)  
     """
     for op in ops_list:
+        if isinstance(op, basestring):
+            op = str2keywords.str2keywords(op)
         if op == 'abs':
             data = np.abs(data)
         elif op == 'log':
@@ -37,9 +48,28 @@ def analysis(data, ops_list, axes=None):
         elif op == 'fft':
             ax = op.keywords.get('axes', None)
             data = np.fft.fftshift(np.fft.fftn(np.fft.ifftshift(data, axes=ax), **op.keywords), axes=ax)
+            if axes:
+                for i in axes:
+                    axes[i] = update_fft_axes(axes[i], forward=True)
         elif op == 'ifft':
             ax = op.keywords.get('axes', None)
             data = np.fft.ifftshift(np.fft.ifftn(np.fft.fftshift(data, axes=ax), **op.keywords), axes=ax)
+            if axes:
+                for i in axes:
+                    axes[i] = update_fft_axes(axes[i], forward=False)
+        elif op == 'mask':
+            data = mask(data, **op.keywords)
+        else:
+            # (*NOT SAFE*) advance user may call functions from any known libaries (only numpy for now)
+            # ********* CAUTIONS *************
+            # 1. be careful with the memory layout of data
+            # 2. the function can only accept one input
+            # 3. there is not way to modify axes data
+            attrs = [x for x in dir(np) if '__' not in x]
+            if op.id in attrs:
+                data = getattr(np, op.id)(data, **op.keywords)
+            else:
+                print('You used ' + op.id + ' but nothing happened!')
     if axes:
         return data, axes
     else:
@@ -47,7 +77,7 @@ def analysis(data, ops_list, axes=None):
 
 # tests
 if __name__ == '__main__':
-    kw = str2keywords.str2keywords('square')
+    kw = str2keywords.str2keywords('fft ')
     a = np.mgrid[:3, :3][0]
     # pass in list of keywords
     a = analysis(a, [kw])
---
+++ para_decompose_2d.py
@@ -0,0 +1,188 @@
+from h5_utilities import *
+import arraymask
+import multiprocessing as mp
+import sys
+import os
+import getopt
+import glob
+import numpy as np
+import analysis
+import copy
+from mpi4py import MPI
+
+
+def print_help():
+    print 'para_decompose_2d.py [options] <InputDir> <OutputDir>'
+    print 'options:'
+    print '  --avg: look into the -savg directory'
+    print '  --env: look into the -senv directory'
+    print '  --blockx=[lower, upper]: set kx between to be zeros'
+    print '  --blocky=[lower, upper]: set ky between to be zeros'
+    print '  --ifftdim=[x1,x2]: which direction for inverse fourier transform'
+    print '  --ocomp=[l,t1,t2]: which component to output'
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+argc = len(sys.argv)
+# # READ COMMAND LINE OPTIONS
+try:
+    opts, args = getopt.gnu_getopt(sys.argv[1:], "h:", ['avg', 'env', 'blockx=', 'blocky=', 'ifftdim=', 'ocomp='])
+except getopt.GetoptError:
+    print_help()
+    sys.exit(2)
+
+if len(args) < 2:
+    print_help()
+    sys.exit(2)
+dirName = args[0]
+outdir = args[1]
+if rank == 0:
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+if outdir[-1] != '/':
+    outdir += '/'
+# # SET DEFAULT OPTIONS
+dir_ext, blockx, blocky, ifftdim, ocomp = '', None, None, None, None
+for opt, arg in opts:
+    if opt == '-h':
+        print_help()
+        sys.exit()
+    elif opt == '--avg':
+        dir_ext = '-savg'
+    elif opt == '--env':
+        dir_ext = '-senv'
+    elif opt == '--blockx':
+        blockx = eval(arg)
+    elif opt == '--blocky':
+        blocky = eval(arg)
+    elif opt == '--ifftdim':
+        ifftdim = arg.lstrip('[').rstrip(']').split(',')
+    elif opt == '--ocomp':
+        ocomp = arg.lower()
+    else:
+        print print_help()
+        sys.exit(2)
+
+# # GET DATA STREAMS
+if rank == 0:
+    elst = {'e1': sorted(glob.glob(dirName + '/MS/FLD/e1' + dir_ext + '/*.h5')),
+            'e2': sorted(glob.glob(dirName + '/MS/FLD/e2' + dir_ext + '/*.h5'))}
+else:
+    elst = None
+elst = comm.bcast(elst, root=0)
+e1, e2 = elst['e1'], elst['e2']
+
+# # divide the task
+total_time = len(e1)
+my_share = total_time / size
+i_begin = rank * my_share
+if rank < (size - 1):
+    i_end = (rank + 1) * my_share
+else:
+    i_end = total_time
+part = total_time / size
+
+# # READ ONE FILE, FIGURE OUT THE AXES INFORMATION AND SET COMMON PARAMETERS
+h5_filename = e1[i_begin]
+h5_template = read_hdf(h5_filename)
+nx = h5_template.shape
+kx, ky = (analysis.update_fft_axes(copy.copy(h5_template.axes[0]), forward=True),
+          analysis.update_fft_axes(copy.copy(h5_template.axes[1]), forward=True))
+kxvec, kyvec = np.meshgrid(kx.get_axis_points(), ky.get_axis_points())
+kamp = np.sqrt(np.square(kxvec) + np.square(kyvec))
+# unit vector
+kxvec, kyvec = np.divide(kxvec, kamp), np.divide(kyvec, kamp)
+if not ifftdim:
+    ift = None
+    h5_template.axes = [kx, ky]
+else:
+    ifftdim = np.array(ifftdim)
+    if 'x2' in ifftdim and 'x1' in ifftdim:
+        ift = 'axes=[0,1]'
+    elif 'x2' not in ifftdim:
+        h5_template.axes[1] = ky
+        ift = 'axes=[0]'
+    else:  # 'x1' not in ifftdim
+        ift = 'axes=[1]'
+# block part of the spectrum
+brg = []
+if blockx:
+    try:
+        for bi in blockx:
+            brg.append(('x1', bi[0], bi[1]))
+    except TypeError:
+        brg.append(('x1', blockx[0], blockx[1]))
+if blocky:
+    try:
+        for bi in blocky:
+            brg.append(('x2', bi[0], bi[1]))
+    except TypeError:
+        brg.append(('x2', blocky[0], blocky[1]))
+
+
+# # FOR EACH TIME STAMP DO SOMETHING
+def foreach_decompose(file_num):
+    h5_output = copy.copy(h5_template)
+    e_filename = e1[file_num]
+    e1file = read_hdf(e_filename)
+    e2file = read_hdf(e2[file_num])
+    e1data = analysis.analysis(e1file.data, ['fft'])
+    e2data = analysis.analysis(e2file.data, ['fft'])
+    h5_output.run_attributes['TIME'][0] = e1file.run_attributes['TIME'][0]
+    h5_output.run_attributes['ITER'][0] = e1file.run_attributes['ITER'][0]
+    if blockx or blocky:
+        e1data = arraymask.mask(e1data, axes=[kx, ky], region=brg)
+        e2data = arraymask.mask(e2data, axes=[kx, ky], region=brg)
+
+    dive = np.multiply(kxvec, e1data) + np.multiply(kyvec, e2data)
+
+    h5_output.data = np.multiply(kxvec, dive)
+    # transverse part, longitudinal part
+    if ift:
+        h5_output.data, e1data = (analysis.analysis(e1data - h5_output.data, ['ifft ' + ift, 'abs']),
+                                  analysis.analysis(h5_output.data, ['ifft ' + ift, 'abs']))
+    else:
+        h5_output.data, e1data = np.abs(e1data - h5_output.data), np.abs(h5_output.data)
+    # if blockx or blocky:
+    #     h5_output.data = arraymask.mask(h5_output.data, axes=[kx, ky], region=brg)
+    if 't1' in ocomp:
+        h5_output.run_attributes['NAME'][0], h5_output.data_attributes['LONG_NAME'] = 'Et1', 'E_{t1}'
+        newname = outdir + 'Et1' + os.path.basename(e_filename)[2:]
+        write_hdf(h5_output, newname)
+
+    h5_output.data = np.multiply(kyvec, dive)
+    if ift:
+        h5_output.data, e2data = (analysis.analysis(e2data - h5_output.data, ['ifft ' + ift, 'abs']),
+                                  analysis.analysis(h5_output.data, ['ifft ' + ift, 'abs']))
+    else:
+        h5_output.data, e2data = np.abs(e2data - h5_output.data), np.abs(h5_output.data)
+    # if blockx or blocky:
+    #     h5_output.data = arraymask.mask(h5_output.data, axes=[kx, ky], region=brg)
+    if 't2' in ocomp:
+        h5_output.run_attributes['NAME'][0], h5_output.data_attributes['LONG_NAME'] = 'Et2', 'E_{t2}'
+        newname = outdir + 'Et2' + os.path.basename(e_filename)[2:]
+        write_hdf(h5_output, newname)
+
+    if 'l' in ocomp:
+        h5_output.data = np.sqrt(np.square(e1data) + np.square(e2data))
+        h5_output.run_attributes['NAME'][0], h5_output.data_attributes['LONG_NAME'] = 'EL', 'E_L'
+        newname = outdir + 'EL' + os.path.basename(e_filename)[2:]
+        write_hdf(h5_output, newname)
+    return e_filename
+
+#TODO: using mpi4py and multiprocess simutaneously may fail on some clusters. fix needed
+if __name__ == '__main__':
+    # threaded version
+    if size == 1:
+        pool = mp.Pool()
+        print pool.map(foreach_decompose, range(i_begin, i_end))
+        pool.close()
+        pool.join()
+    else:
+        # serial/mpi version
+        for time_i in range(i_begin, i_end):
+            foreach_decompose(time_i)
+
+comm.Barrier()
+comm.Disconnect()
