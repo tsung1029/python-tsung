29c49b36688c57938276944ab20ce8b2b4d6902b
Han Wen <hanwen@ucla.edu>
2017-10-10 13:12:46 -0700

    adjuct para_sum for new hdf_data

--- para_sum.py
+++ para_sum.py
@@ -10,10 +10,11 @@ from mpi4py import MPI
 def print_help():
     print 'para_sum.py [options] <InputDir> <OutputName>'
     print 'options:'
-    print '  --sq: sum of squared values (default)'
-    print '  --abs: sum of absolute values'
+    print '  --sq: sum of squared values, default is naive sum'
+    print '  --abs: sum of absolute values, default is naive sum'
     print '  -x: sum over x'
     print '  -y: sum over y (default)'
+
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -28,19 +29,19 @@ except getopt.GetoptError:
 if len(args) < 2:
     print_help()
     sys.exit(2)
-dirName = args[0]
-outFilename = args[1]
+dirName, outFilename = args[0], args[1]
 
-do_sq_sum = True
-sum_in_y = True
+sum_type, sum_in_y = '+', True
 for opt, arg in opts:
     if opt == '-h':
         print_help()
         sys.exit()
     elif opt == '-x':
         sum_in_y = False
+    elif opt == '--sq':
+        sum_type = 'sq'
     elif opt == '--abs':
-        do_sq_sum = False
+        sum_type = 'abs'
 
 fileList = sorted(glob.glob(dirName + '/*.h5'))
 total_time = len(fileList)
@@ -52,55 +53,47 @@ else:
     i_end = total_time
 part = total_time / size
 
-h5_filename = fileList[1]
+h5_filename = fileList[0]
 h5_data = read_hdf(h5_filename)
-array_dims = h5_data.shape
-nx = array_dims[0]
-ny = array_dims[1]
-time_step = h5_data.run_attributes['TIME'][0]
+nx = h5_data.shape
+t0 = h5_data.run_attributes['TIME'][0]
+h5_data = read_hdf(fileList[1])
+t1 = h5_data.run_attributes['TIME'][0]
 h5_output = hdf_data()
 if sum_in_y:
-    nd = ny
-    nds = nx
     idx = 0
 else:
-    nd = nx
-    nds = ny
     idx = 1
-h5_output.shape = [total_time, nd]
-print 'nx=' + repr(nx)
-print 'ny=' + repr(ny)
-print 'time_step=' + repr(time_step)
-print 'total_time=' + repr(total_time)
+nd = nx[-(idx+1)]
+
 h5_output.data = numpy.zeros((total_time, nd))
-# if rank == 0:
+h5_output.shape = h5_output.data.shape
 total = numpy.zeros((total_time, nd))
-h5_output.axes = [data_basic_axis(0, h5_data.axes[idx].axis_min, h5_data.axes[idx].axis_max, nd),
-                  data_basic_axis(1, 0.0, (time_step * total_time - 1), total_time)]
+h5_output.axes = [data_basic_axis(1, t0, (t1 - t0) * (total_time - 1), total_time), h5_data.axes[-(idx+1)]]
 h5_output.run_attributes['TIME'] = 0.0
-# h5_output.run_attributes['UNITS']=h5_data.run_attributes['UNITS']
-h5_output.axes[0].attributes['LONG_NAME'] = h5_data.axes[idx].attributes['LONG_NAME']
-h5_output.axes[0].attributes['UNITS'] = h5_data.axes[idx].attributes['UNITS']
-h5_output.axes[1].attributes['LONG_NAME'] = 'TIME'
-h5_output.axes[1].attributes['UNITS'] = '1/\omega_p'
+h5_output.axes[0].attributes['LONG_NAME'] = 'TIME'
+h5_output.axes[0].attributes['UNITS'] = '1/\omega_p'
 
 
 # file_number = 0
 # for file_number in range(i_begin, i_end):
 def foreach_sum(file_num):
     h5_fname = fileList[file_num]
-    print h5_fname
+    # print h5_fname
     h5data = read_hdf(h5_fname)
-    if do_sq_sum:
-        temp = numpy.sum((h5data.data * h5data.data), axis=idx) / nds
-    else:  # '--sq'
-        temp = numpy.sum(numpy.abs(h5data.data), axis=idx) / nds
-    return temp[1:nd]
+    if sum_type == 'sq':
+        temp = numpy.sum((h5data.data * h5data.data), axis=idx)
+    elif sum_type == '+':
+        temp = numpy.sum(h5data.data, axis=idx)
+    else:  # '--abs'
+        temp = numpy.sum(numpy.abs(h5data.data), axis=idx)
+    return temp
+
 
 if __name__ == '__main__':
     pool = mp.Pool()
     time_i = range(i_begin, i_end)
-    h5_output.data[time_i, 1:nd] = numpy.array(pool.map(foreach_sum, time_i))
+    h5_output.data[time_i, :] = numpy.array(pool.map(foreach_sum, time_i))
     pool.close()
     pool.join()
 
