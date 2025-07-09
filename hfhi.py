bd3c387796508c9a72ea4ae2e58c99577e014fea
Frank S Tsung <tsung@physics.ucla.edu>
2025-07-09 15:04:34 -0700

    Update hfhi.py
    Added functions to calculate collisionality

--- hfhi.py
+++ hfhi.py
@@ -1,5 +1,6 @@
 import numpy
 import cmath
+import numpy as np
 mecgs = 9.1094e-28
 qecgs = 4.8032e-10
 ccgs = 29979000000
@@ -67,6 +68,161 @@ def cmult(tev,ionefour,lnmic,lambdamic):
 def cmult_srs(ionefour,lnmic,lambdamic):
     result=(ionefour**0.75*lambdamic**0.5)*(lnmic)/155.0
     return result
+
+def lambdadebye(density,te):
+    result = vth(te)/omegap(density)
+    return result
+
+def plasma_param(density,te):
+    result = lambdadebye(density,te)**3*density
+    return result
+
+
+def coulomb_logarithm(n_e, T_e, Z=1):
+    """
+    Calculate the Coulomb logarithm lnΛ for a plasma.
+    
+    Parameters:
+        n_e : float
+            Electron density in cm^-3
+        T_e : float
+            Electron temperature in eV
+        Z   : int or float
+            Ion charge number (default is 1)
+    
+    Returns:
+        ln_Lambda : float
+            Coulomb logarithm (dimensionless)
+    """
+    # Physical constants (SI)
+    e = 1.602e-19       # Elementary charge [C]
+    epsilon_0 = 8.854e-12  # Vacuum permittivity [F/m]
+    k_B = 1.381e-23     # Boltzmann constant [J/K]
+    T_J = T_e * e       # Convert eV to Joules
+
+    n_e = n_e * 1e6  # Convert from cm^-3 to m^-3
+    # Debye length (b_max)
+    lambda_D = np.sqrt(epsilon_0 * T_J / (n_e * e**2))
+
+    # Closest approach distance (b_min)
+    b_min = Z * e**2 / (4 * np.pi * epsilon_0 * T_J)
+
+    # Coulomb logarithm
+    ln_Lambda = np.log(lambda_D / b_min)
+
+    return ln_Lambda
+
+
+
+
+def coll_ei(n_e, T_e, Z=1):
+    """
+    Calculate the electron-ion Coulomb collision frequency ν_ei.
+
+    Parameters:
+        n_e : float
+            Electron density in cm^-3
+        T_e : float
+            Electron temperature in eV
+        Z   : int or float
+            Ion charge number (default 1)
+
+    Returns:
+        nu_ei : float
+            Electron-ion collision frequency in Hz
+    """
+
+    n_e = n_e * 1e6  # Convert from cm^-3 to m^-3
+    # Physical constants
+    e = 1.602e-19       # Elementary charge [C]
+    epsilon_0 = 8.854e-12  # Vacuum permittivity [F/m]
+    k_B = 1.381e-23     # Boltzmann constant [J/K]
+    m_e = 9.109e-31     # Electron mass [kg]
+    T_J = T_e * e       # Convert eV to Joules
+
+    # Compute Coulomb logarithm (basic version)
+    lambda_D = np.sqrt(epsilon_0 * T_J / (n_e * e**2))
+    b_min = Z * e**2 / (4 * np.pi * epsilon_0 * T_J)
+    ln_Lambda = np.log(lambda_D / b_min)
+
+    # Electron-ion collision frequency
+    prefactor = (4 * np.sqrt(2 * np.pi)) / 3
+    result = prefactor * n_e * Z**2 * e**4 * ln_Lambda / \
+            (m_e**0.5 * epsilon_0**2 * T_J**1.5)
+
+    return result
+
+def coll_time_ee(ne_cm3, Te_eV):
+    """
+    Calculate the electron-electron collision time in CGS units.
+    
+    Parameters:
+    -----------
+    Te_eV : float
+        Electron temperature in electron volts (eV)
+    ne_cm3 : float
+        Electron number density in cm^-3
+    
+    Returns:
+    --------
+    tau_ee : float
+        Electron-electron collision time in seconds (s)
+    """
+
+    # Physical constants (CGS)
+    e = 4.8032e-10     # statC
+    k_B = 1.6022e-12   # erg/eV
+    me = 9.1094e-28    # g
+    pi = np.pi
+
+    # Temperature in erg
+    Te = Te_eV * k_B
+
+    # Estimate Coulomb logarithm
+    lambda_D = np.sqrt(Te / (4 * pi * ne_cm3 * e**2))
+    b_min = e**2 / Te
+    ln_Lambda = np.log(lambda_D / b_min)
+
+    # Electron-electron collision time (seconds)
+    tau_ee = (3 * np.sqrt(me) * Te**1.5) / (4 * np.sqrt(2) * pi * ne_cm3 * e**4 * ln_Lambda)
+
+    return tau_ee
+
+def nu_ee(ne_cm3, Te_eV):
+    """
+    Calculate the electron-electron collision frequency in CGS units.
+    
+    Parameters:
+    -----------
+    Te_eV : float
+        Electron temperature in electron volts (eV)
+    ne_cm3 : float
+        Electron number density in cm^-3
+    
+    Returns:
+    --------
+    nuee : float
+        Electron-electron collision frequency in s^-1
+    """
+
+    # Physical constants (CGS)
+    e = 4.8032e-10     # statC
+    k_B = 1.6022e-12   # erg/eV
+    me = 9.1094e-28    # g
+    pi = np.pi
+
+    # Convert temperature to erg
+    Te = Te_eV * k_B
+
+    # Estimate Coulomb logarithm
+    lambda_D = np.sqrt(Te / (4 * pi * ne_cm3 * e**2))
+    b_min = e**2 / Te
+    ln_Lambda = np.log(lambda_D / b_min)
+
+    # Collision frequency in s^-1
+    nuee = (4 * np.sqrt(2) * pi * ne_cm3 * e**4 * ln_Lambda) / (3 * np.sqrt(me) * Te**1.5)
+
+    return nuee
 
 
 
